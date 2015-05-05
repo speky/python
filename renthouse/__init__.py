7bb57896fc4f2483c0859e0bf09f72080b915d84
Zsolt Specker <zsolt.specker@gmail.com>
2015-05-05 14:52:56 +0200

    get max page number

--- renthouse/__init__.py
+++ renthouse/__init__.py
@@ -7,9 +7,18 @@ index_url = 'http://alberlet.hu/kiado_alberlet'
 
 #links = soup.select('div.video-summary-data a[href^=/video]')
 
+def get_max_page_number():
+    response = requests.get(index_url+"/page:1000")
+    soup = bs4.BeautifulSoup(response.text)
+    max_page = soup.find('li', attrs={'class' : 'current'})
+    return max_page.text
+
+
 def get_video_page_urls():
     response = requests.get(index_url)
     soup = bs4.BeautifulSoup(response.text)
+
+    print get_max_page_number()
     links = []
     #return [a.attrs["href"] for a in soup.select('div.listing-image')]
     #soup.find_all('a', href=True):
@@ -18,6 +27,7 @@ def get_video_page_urls():
     #        continue
     #     print "Found the URL:", links['href']
     divs = soup.find_all('div', attrs={'class' : 'listing-image'})
+
     for div in divs:
         print div.find('a')['href']
 
