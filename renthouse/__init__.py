835408a7f2c05f326cf14c7738520afd03272026
Zsolt Specker <zsolt.specker@gmail.com>
2015-05-06 15:42:58 +0200

    widgets

--- renthouse/__init__.py
+++ renthouse/__init__.py
@@ -3,49 +3,87 @@
 __author__ = 'speky'
 import alberlet
 import Tkinter
+from Tkinter import *
 
-window = Tkinter.Tk()
+class Gui(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.master = master
+        self.init_window()
 
-label = Tkinter.Label(window, text="label")
-Lb1 = Tkinter.Listbox(window, selectmode=Tkinter.MULTIPLE, height=3)
+    def init_window(self):
+        self.master.title("Awsome kereso")
+        # get screen width and height
+        ws = self.master.winfo_screenwidth()#This value is the width of the screen
+        hs = self.master.winfo_screenheight()#This is the height of the screen
+        #make my screen dimensions work
+        w = 200 #The value of the width
+        h = 200 #The value of the height of the window
+        # calculate position x, y
+        x = (ws/2) - (w/2)
+        y = (hs/2) - (h/2)
+        #This is responsible for setting the dimensions of the screen and where it is placed
+        self.master.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-def init_window():
-    window.title("Awsome kereso")
-    #window.geometry("500x500")
-    label.pack()
+        self.label = Tkinter.Label(self.master, text="label")
+        self.Lb1 = Tkinter.Listbox(self.master, selectmode=Tkinter.MULTIPLE, height=3)
+        self.label.pack()
 
-    Lb1.insert(1, "Python")
-    Lb1.insert(2, "Perl")
-    Lb1.insert(3, "C")
-    Lb1.bind('<<ListboxSelect>>', onselect)
-    Lb1.pack()
+        self.Lb1.insert(1, "Python")
+        self.Lb1.insert(2, "Perl")
+        self.Lb1.insert(3, "C")
+        self.Lb1.bind('<<ListboxSelect>>', self.onselect)
+        self.Lb1.pack()
+        #self.Lb1.select_set(0) # sets the first element
+        self.Lb1.select_set(0, END)
 
-    button = Tkinter.Button(window, text="click", command=callback_click)
-    button.pack()
+        button = Tkinter.Button(self.master, text="click", command=self.callback_click)
+        button.pack()
 
-def onselect(evt):
-    # Note here that Tkinter passes an event object to onselect()
-    w = evt.widget
-    index = int(w.curselection()[0])
-    value = w.get(index)
-    print 'You selected item %d: "%s"' % (index, value)
+        self.e = Entry(self.master, width=5)
+        self.e.pack()
+        self.e.delete(0, END)
+        self.e.insert(0, "25")
 
-def callback_click():
-    label.configure(text="click")
-    _values = [Lb1.get(idx) for idx in Lb1.curselection()]
-    _message = ', '.join(_values)
-    show_message(_message)
-
-
-def show_message(message):
-    _messageWindow = Tkinter.Tk()
-    _msg = Tkinter.Message(_messageWindow, text = message, width = 200)
-    _button = Tkinter.Button(_messageWindow, text='Ok', command = _messageWindow.destroy)
-    _msg.pack()
-    _button.pack()
-    _messageWindow.mainloop()
-
-init_window()
+        self.var = IntVar()
+        self.c = Checkbutton(
+            self.master,
+            text="Kisallat",
+            variable = self.var,
+            command = self.cb)
+        self.c.select()
+        self.c.pack()
 
 
-window.mainloop()
+    def cb(self):
+        print "variable is", self.var.get()
+
+    def onselect(self, evt):
+        # Note here that Tkinter passes an event object to onselect()
+        w = evt.widget
+        index = int(w.curselection()[0])
+        value = w.get(index)
+        print 'You selected item %d: "%s"' % (index, value)
+
+    def callback_click(self):
+        self.label.configure(text="click")
+        _values = [self.Lb1.get(idx) for idx in self.Lb1.curselection()]
+        _message = ', '.join(_values)
+        print self.e.get()
+        self.show_message(_message)
+
+
+    def show_message(self, message):
+        _messageWindow = Tkinter.Tk()
+        _msg = Tkinter.Message(_messageWindow, text = message, width = 200)
+        _button = Tkinter.Button(_messageWindow, text='Ok', command = _messageWindow.destroy)
+        _msg.pack()
+        _button.pack()
+        _messageWindow.mainloop()
+
+if __name__ == '__main__':
+    root = Tkinter.Tk()
+    gui = Gui(master=root)
+    gui.mainloop()
+
