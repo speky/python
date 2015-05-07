332b966b379e9c5b183af9b3e49cd968e6f01d1f
Zsolt Specker <zsolt.specker@gmail.com>
2015-05-07 17:24:16 +0200

    improve data access

--- renthouse/__init__.py
+++ renthouse/__init__.py
@@ -1,10 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 __author__ = 'speky'
 import alberlet
 from alberlet import *
 import tkinter
 from tkinter import *
+
+from geopy.geocoders import Nominatim
+from geopy.distance import vincenty
+
 
 class Gui(Frame):
     def __init__(self, master=None):
@@ -16,19 +20,19 @@ class Gui(Frame):
     def init_window(self):
         self.master.title("Awsome kereso")
         # get screen width and height
-        ws = self.master.winfo_screenwidth()#This value is the width of the screen
-        hs = self.master.winfo_screenheight()#This is the height of the screen
-        #make my screen dimensions work
-        w = 200 #The value of the width
-        h = 200 #The value of the height of the window
+        ws = self.master.winfo_screenwidth()  # This value is the width of the screen
+        hs = self.master.winfo_screenheight()  # This is the height of the screen
+        # make my screen dimensions work
+        w = 200  # The value of the width
+        h = 200  # The value of the height of the window
         # calculate position x, y
-        x = (ws/2) - (w/2)
-        y = (hs/2) - (h/2)
-        #This is responsible for setting the dimensions of the screen and where it is placed
+        x = (ws / 2) - (w / 2)
+        y = (hs / 2) - (h / 2)
+        # This is responsible for setting the dimensions of the screen and where it is placed
         self.master.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
         self.label = Label(self.master, text="label")
-        self.Lb1 = Listbox(self.master, selectmode = MULTIPLE, height=3)
+        self.Lb1 = Listbox(self.master, selectmode=MULTIPLE, height=3)
         self.label.pack()
 
         self.Lb1.insert(1, "Python")
@@ -36,7 +40,7 @@ class Gui(Frame):
         self.Lb1.insert(3, "C")
         self.Lb1.bind('<<ListboxSelect>>', self.onselect)
         self.Lb1.pack()
-        #self.Lb1.select_set(0) # sets the first element
+        # self.Lb1.select_set(0) # sets the first element
         self.Lb1.select_set(0, END)
 
         button = Button(self.master, text="click", command=self.callback_click)
@@ -51,43 +55,54 @@ class Gui(Frame):
         self.c = Checkbutton(
             self.master,
             text="Kisallat",
-            variable = self.var,
-            command = self.cb)
+            variable=self.var,
+            command=self.cb)
         self.c.select()
         self.c.pack()
-
 
     def save_file(self):
         with open("Output.txt", "w") as text_file:
             text_file.write("Purchase Amount: {0}\n".format(123345))
 
     def cb(self):
-        print ("variable is " + str(self.var.get()))
+        print("variable is " + str(self.var.get()))
 
     def onselect(self, evt):
         # Note here that Tkinter passes an event object to onselect()
         w = evt.widget
         index = int(w.curselection()[0])
         value = w.get(index)
-        print ('You selected item %d: "%s"' % (index, value))
+        print('You selected item %d: "%s"' % (index, value))
+
+    def show_dist(self):
+        geolocator = Nominatim()
+        location = geolocator.geocode("Budapest, XXIII. Kerület Majosháza utca")
+        location2 = geolocator.geocode("Holló utca Budapest, VII. Kerület ")
+        print(location.address)
+        loc1 = (location.latitude, location.longitude)
+        loc2 = (location2.latitude, location2.longitude)
+        print(vincenty(loc1, loc2).meters)
+        return location.address
 
     def callback_click(self):
         self.label.configure(text="click")
         _values = [self.Lb1.get(idx) for idx in self.Lb1.curselection()]
         _message = ', '.join(_values)
 
-        self.show_message(alberlet.get_max_page_number())
-
+        # self.show_message(alberlet.get_max_page_number())
+        self.show_message(self.show_dist())
 
     def show_message(self, message):
         _messageWindow = Tk()
-        _msg = Message(_messageWindow, text = message, width = 200)
-        _button = Button(_messageWindow, text='Ok', command = _messageWindow.destroy)
+        _msg = Message(_messageWindow, text=message, width=200)
+        _button = Button(_messageWindow, text='Ok', command=_messageWindow.destroy)
         _msg.pack()
         _button.pack()
         _messageWindow.mainloop()
 
+
 if __name__ == '__main__':
+    alberlet.get_page_urls()
     root = Tk()
     gui = Gui(master=root)
     gui.mainloop()
--- renthouse/alberlet.py
+++ renthouse/alberlet.py
@@ -3,39 +3,78 @@ import requests
 import bs4
 
 root_url = 'http://alberlet.hu'
-index_url = root_url + '/kiado_alberlet/berleti-dij:0-50-ezer-ft/ingatlan-tipus:lakas/ingatlan:tegla/limit:48'
+index_url = root_url + '/kiado_alberlet/ingatlan-tipus:lakas/kerulet:xxiii/megye:budapest/keres:advanced/limit:32'
+# '/kiado_alberlet/berleti-dij:0-50-ezer-ft/ingatlan-tipus:lakas/ingatlan:tegla/limit:48'
 
 def get_max_page_number():
-    response = requests.get(index_url+"/page:last")
-    soup = bs4.BeautifulSoup(response.text)
-    max = soup.find('li', attrs={'class' : 'current'})
-    return max.text
+    _response = requests.get(index_url + "/page:last")
+    _soup = bs4.BeautifulSoup(_response.text)
+    _max = _soup.find('li', attrs={'class': 'current'})
+    return _max.text
 
-def get_video_page_urls(uri):
-    response = requests.get(uri)
+
+def get_link(div):
+    address = div.find('div', attrs={'class': 'box-grid-head'})
+    _link = address.find('a')['href']
+    if _link.startswith('/'):
+        return root_url + _link
+    return _link
+
+
+def get_address(div):
+    _district = div.find('h4', attrs={'class': 'city-with-info'}).text
+    _address = div.find('div', attrs={'class': 'box-grid-head'})
+    _street = _address.find('a').text
+    return _district.strip() + ' ' + _street
+
+
+def get_price(div):
+    _p = div.find('p', attrs={'class': 'listing-rent'})
+    _price = _p.find('b').text
+    _price = ''.join(_price.split())
+    # remove HUF string from the end
+    _price = _price[:-3]
+    return _price
+
+
+def get_area_size(div):
+    _p = div.find('p', attrs={'class': 'listing-rooms'})
+    _size = _p.find('b').text
+    _size = ''.join(_size.split())
+    # remove m2 string from the end
+    _size = _size[:-2]
+    return _size
+
+
+def get_page_urls():
+    response = requests.get(index_url)
     soup = bs4.BeautifulSoup(response.text)
 
-    links = []
-    divs = soup.find_all('div', attrs={'class' : 'listing-image'})
+    # find the rent box div
+    divs = soup.find_all('div', attrs={'class': 'listing-item'})
+    #print(divs)
+
+    rentHouses = {} # dictionary
 
     for div in divs:
-        link = div.find('a')['href']
-        #print link
-        if link.startswith('/'):
-            links.append(root_url + link)
-            #print link
-            #print root_url+link
-        else:
-           links.append(link)
-    return links
+       # link = get_link(div)
+       # street = get_address(div)
+       # price = get_price(div)
+       # size = get_area_size(div)
+        rentHouses['abc'] = ['street1', 'price', 'size1', 2]
+        rentHouses['dfg'] = ['street2', 'price', 'size2', 2]
+        print(rentHouses)
+        for key, values in rentHouses.items():
+            print(values[2])
 
-#LAST_PAGE = int(get_max_page_number())
-#print LAST_PAGE
-linkLists = []
-#for pageNumber in range(1, LAST_PAGE + 1):
-#    print "pageNumber: " + str(pageNumber)
- #   linkLists.append(get_video_page_urls(index_url + '/page:' + str(pageNumber)))
+        # LAST_PAGE = int(get_max_page_number())
+        # print LAST_PAGE
+        # linkLists = []
 
-for list in linkLists:
-    for elem in list:
-       print (elem)
+        #for pageNumber in range(1, LAST_PAGE + 1):
+        #    print "pageNumber: " + str(pageNumber)
+        #   linkLists.append(get_video_page_urls(index_url + '/page:' + str(pageNumber)))
+
+        #for list in linkLists:
+        #    for elem in list:
+        #        print(elem)
